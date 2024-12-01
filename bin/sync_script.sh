beca9c86e96a61f5034d48f7de2df2dc87798c0e
IA (naruto522ru) <itachi522ru@gmail.com>
2024-12-01 09:15:35 +0300

    add lost utils

--- bin/sync_script.sh
+++ bin/sync_script.sh
@@ -13,5 +13,5 @@ else
     echo "$(date +"%d.%m.%y %H:%M:%S"): Скрипт ${NAME_SCRIPT} недоступен. Не скачиваем его на GitHub,оставляем старый\!"
 fi
 # Условие проверяет не пуст ли сам файл. Дело в том, что файл может существовать на удаленном сервере, но может быть пустым от такого первое if не спасает. Если такой будет, то делаем git restore , чтобы не закоммить в git пустой файл.
-if [[ -s ${PATH_DOWNLOAD}${NAME_SCRIPT} ]]; then echo "No empty $NAME_SCRIPT" && cat ${PATH_DOWNLOAD}${NAME_SCRIPT} > ${NAME_SCRIPT}_jq;else echo -e "\e[1;33mEmpty\033[0m $NAME_SCRIPT" && git restore ./$NAME_SCRIPT; fi
+if [[ -s ${PATH_DOWNLOAD}${NAME_SCRIPT} ]]; then echo "No empty $NAME_SCRIPT" && cat ${PATH_DOWNLOAD}${NAME_SCRIPT} | jq > ${NAME_SCRIPT}_jq;else echo -e "\e[1;33mEmpty\033[0m $NAME_SCRIPT" && git restore ./$NAME_SCRIPT; fi
 exit 0
